a4366b94d69a3ca6bc4f1b384a920e17faf401d4
ak1803 <jakshay18397@gmail.com>
2025-08-13 12:58:47 +0530

    Favicon

--- client/components/Dashboard.tsx
+++ client/components/Dashboard.tsx
@@ -25,7 +25,7 @@ export function Dashboard({ className }: DashboardProps) {
 
         {/* Content */}
         <div className="flex-1 p-3 sm:p-4 lg:p-8 overflow-auto animate-slide-in-up">
-          <div className="max-w-[1164px] mx-auto space-y-6 sm:space-y-8 lg:space-y-12">
+          <div className="max-w-[100%] mx-auto space-y-6 sm:space-y-8 lg:space-y-12">
             {/* Title Section */}
             <div className="space-y-1 sm:space-y-2 animate-slide-in-up">
               <h1 className="text-white text-xl sm:text-2xl font-bold font-['Plus_Jakarta_Sans']">
